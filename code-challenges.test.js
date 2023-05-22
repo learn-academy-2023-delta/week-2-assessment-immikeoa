9e1083d0342355e9b2c36c61c7632fdef0b8dec8
immikeoa <immikeoa@gmail.com>
2023-05-22 08:16:47 -0400

    final push.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -26,42 +26,110 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
-
+// describe("divisibleBy3", () => {
+//     it("Takes in an object and returns if divisible by three.", () => {
+//       const object1 = { number: 15 }
+//       const object2 = { number: 0 }
+//       const object3 = { number: -7 }
+//       expect(divisibleBy3(object1.number)).toEqual("is divisible by three")
+//       expect(divisibleBy3(object2.number)).toEqual("is divisible by three")
+//       expect(divisibleBy3(object3.number)).toEqual("is not divisible by three")
+//     })
+//   }) //● divisibleBy3 › Takes in an object and returns if divisible by three.
+  
 // b) Create the function that makes the test pass.
-
+const divisibleBy3 = (object) => {
+      if (object % 3 === 0){
+        return "is divisible by three"
+      } else if (object % 3 !== 0){
+        return "is not divisible by three"
+      }
+}  
 // Pseudo code:
+// Input: Object
+// Output: "if the Number is divible by 3"
+// Create a function named divisibleBy3
+// takes in an obect key value which will be a number as an argument
+// crate a conditional if/ else if statement 
+// with the modulo % check if each argument is divisible by three. 
+// return a string saying if it is or not
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
-
+// describe("capArray", () => {
+//     it("takes in an array of words and returns an array with all the words capitalized.", () => {
+//         const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+//         const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+//       expect(capArray(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+//       expect(capArray(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+//     })
+//   }) //capArray › takes in an array of words and returns an array with all the words capitalized.
 // b) Create the function that makes the test pass.
-
+const capArray = (array) => {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i].charAt(0).toUpperCase());
+  }
+  return newArray;
+}; // i got lost trying to find the issue. later i added .charAt method make sure it was the first lleter capitalized. Array [
+  // -   "Streetlamp",
+  // -   "Potato",
+  // -   "Teeth",
+  // -   "Conclusion",
+  // -   "Nephew",
+  // +   "S",
+  // +   "P",
+  // +   "T",
+  // +   "C",
+  // +   "N",
+  //   ]
 // Pseudo code:
+// create a function named capArray
+// it takes an array as an arument
+// use a for loop to iterate throught each index
+// capitalized each index with the .toUpperCase method
+// return the array
+// Input: an array of words
+// Output: an array with the strings capitalized
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
-
 // a) Create a test with expect statements for each of the variables provided.
+// describe("finFirstVowelIndex", () => {
+//     it("takes in a string and logs the index of the first vowel.", () => {
+//         const vowelTester1 = "learn"
+//         const vowelTester2 = "academy"
+//         const vowelTester3 = "challenges"
+//       expect(findFirstVowelIndex(vowelTester1)).toEqual(1)
+//       expect(findFirstVowelIndex(vowelTester2)).toEqual(0)
+//       expect(findFirstVowelIndex(vowelTester3)).toEqual(2)
+//     })
+//   }) // ● finFirstVowelIndex › takes in a string and logs the index of the first vowel.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+  // b) Create the function that makes the test pass.
+const findFirstVowelIndex = (str) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const lowerCaseStr = str.toLowerCase();
+  
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    if (vowels.includes(lowerCaseStr[i])) {
+       return console.log(i);
+    
+    }
 
-// b) Create the function that makes the test pass.
+  }
+} // I tried changing the return several ways but it stills gives me undefinned
+// expect(received).toEqual(expected) // deep equality
+
+// Expected: 1
+// Received: undefined
 
 // Pseudo code:
+// create a function name findFirstVowelIndex takes in an string as argument
+// create an array to hold vowels to compare to
+// crate vaariable that is equal to the passed in string lowercased using the toLowercase method
+// create a for loop to iterate throught each index the new string
+// use the .includes() method to check the array of vowels
+// return the index of the passed in string that matches the array
+// Input : a string 
+// Output : returns the index of the first vowel
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,54 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: a parameter is a variable set in the the function that will accept an argument passed in when the the function is invoked
 
-Researched answer:
+Researched answer: A parameter is a variable or a placeholder used in the function/method definition to represent a value that the function expects to receive. An argument is a specific value or expression passed to a function/method when it is called or invoked.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: It takes the value of each index in the array been map. 
 
-Researched answer:
+Researched answer: For a .map() it takes a callback function as its parameter, which is called for each element in the array:
+           currentValue (required): The current element being processed in the array.
+           index (optional): The index of the current element being processed in the array.
+           array (optional): The array on which the .map() method was called.
+
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: .map method will iterarte throught the array and output the array with the same lenght. the .filter method iterates throught the array but output an array with different lenght due to the condition set in method. Only the value that passed the conditional will pass to the new array.
 
-Researched answer:
+Researched answer: They both take iterate throught an array and output a new on. A map outputs a new array with same lenght as the original while the .filter outputs a different lenght.
 
 1. What is iteration?
 
-Your answer:
+Your answer: the repetition of task in programming
 
-Researched answer:
+Researched answer: Iteration refers to the process of repeatedly performing a set of instructions or operations. In programming, iteration is often used to perform tasks on a collection of elements, such as iterating over an array or a list, to process each element individually.
 
 1. What is the difference between a function and a method?
 
-Your answer:
+Your answer: a funtion is a block of code created by the programmer that can be invoked many time. A method is a built in function in the proggraming language.
 
-Researched answer:
+Researched answer: A function is a block of code that performs a specific task or set of instructions. It is a self-contained unit of code that can be called or invoked from different parts of a program. Functions are generally standalone and not associated with any particular object or class. A method is a function that is associated with an object or a class. It is a behavior or action that an object or class can perform.
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: The ability to access a variable within a self contain block of code.
 
-Researched answer:
+Researched answer: s a JavaScript behavior where variable and function declarations are moved to the top of their containing scope during the compilation phase, while still maintaining their original order in the code. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Component:
+1. Component: is a modular and reusable piece of code that encapsulates specific functionality and can be combined with other components to build larger software systems.
 
-2. Spread operator:
+2. Spread operator: it allows an iterable (such as an array, string, or object) to be expanded into individual elements. It provides an easy and concise way to manipulate and combine arrays, clone objects, and pass multiple arguments to functions.
 
-3. React state:
+3. React state: In React, state is a built-in feature that allows components to manage and store data that can change over time. State represents the current state or values of a component and is used to determine how the component should render and behave in response to user interactions, data fetching, or other events.
 
-4. React props:
+4. React props: are a way to pass data from a parent component to its child components. Props allow components to be configurable and customizable, enabling the composition and reuse of components with different data and behaviors.
 
-5. DOM events:
+5. DOM events: DOM events in web development refer to actions or occurrences that happen in the Document Object Model (DOM) of an HTML document. These events can be triggered by user interactions, like clicking a button or submitting a form, or by other actions such as the page finishing loading or a timer reaching a specified interval.
